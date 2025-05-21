70a52366ce04d788cd32e06441656aef08482572
Rohith Gurram <ajay@Mac.lan>
2025-05-21 16:13:08 -0500

    Added user model,repository and controller

---
+++ src/main/java/com/tripmate/tripmate/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.tripmate.tripmate.controller;
+
+import com.tripmate.tripmate.model.User;
+import com.tripmate.tripmate.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
---
+++ src/main/java/com/tripmate/tripmate/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tripmate.tripmate.repository;
+
+import com.tripmate.tripmate.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    User findByEmail(String email);
+}
